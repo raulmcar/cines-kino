9c4ce7b37b3f3d00665efedec39b5ab1a4e24d8e
Raul <raulmcar@hotmail.com>
2025-03-16 22:22:17 +0100

    Primer dia haciendo cosillas

--- instalar.php
+++ instalar.php
@@ -1,5 +1,37 @@
 <?php
+    session_start();
+    $usuario = "root";
+    $password = "Furciademierda4";
+    $host = "localhost";
 
+    try {
+        $bdConexion = new PDO("mysql:host=$host", $usuario, $password);
+
+        $bdConexion->exec("CREATE DATABASE IF NOT EXISTS cine;");
+
+        $bdConexion->exec("USE cine;");
+
+        $bdConexion->beginTransaction();
+
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS usuario(
+            id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+            nombre VARCHAR(50),
+            apellidos VARCHAR(100),
+            email VARCHAR(100),
+            contrasena VARCHAR(50),
+            telefono INT(9),
+            dni VARCHAR(9),
+            fecha_nacimiento VARCHAR(10),
+            tipo_usuario VARCHAR(10));");
+
+        
+
+
+
+
+
+
+    }
 
 
 
