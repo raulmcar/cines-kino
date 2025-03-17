df6ed67ebb5d14f5c6ce4a2aeaa2378f26e2c4db
Raul <raulmcar@hotmail.com>
2025-03-17 17:16:50 +0100

    He avanzado en el index y en el instalar bbdd

--- index.php
+++ index.php
@@ -21,14 +21,64 @@
             <a class="navbar-brand" href="">
                 <img src="./imagenes/logo.png" alt="Bootstrap" width="110" height="80">
             </a>
-            <a class="navbar-brand fs-4 fw-semibold" href="#">Cartelera</a>
-            <button type="button" class="btn btn-warning text-black rounded-circle">
+            <a class="navbar-brand fs-4 fw-semibold mx-auto" href="#">Cartelera</a>
+        <div class="dropdown">
+            <button class="btn btn-warning text-black rounded-circle dropdown-toggle" id="loginDropdown" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="bi bi-person-fill fs-3"></i>
             </button>
+            <div class="dropdown-menu dropdown-menu-end p-3 shadow-lg" style="width: 250px;">
+                <h6 class="text-center fw-bold">Iniciar Sesión</h6>
+                <form>
+                    <div class="mb-2">
+                        <label for="email" class="form-label">Correo electrónico</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="mb-2">
+                        <label for="password" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-dark w-100">Entrar</button>
+                </form>
+                <p class="text-center mb-0 mt-1">
+                    <a href="#" class="text-decoration-none">¿No tienes cuenta? Regístrate</a>
+                </p>
+            </div>
+        </div>
         </div>
     </nav>
 
-    
+    <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./imagenes/estrenos semanales.png" class="d-block w-100 imagen_carrusel" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./imagenes/snacks.png" class="d-block w-100 imagen_carrusel" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./imagenes/cinesalas.png" class="d-block w-100 imagen_carrusel" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- instalar.php
+++ instalar.php
@@ -24,9 +24,53 @@
             fecha_nacimiento VARCHAR(10),
             tipo_usuario VARCHAR(10));");
 
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS reserva(
+            id_reserva INT PRIMARY KEY AUTO_INCREMENT,
+            id_usuario INT(5),
+            id_sesion INT(5),
+            id_asiento_sesion(5),
+            precio FLOAT(10),
+            fecha_reserva VARCHAR(10));");
+
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS pago(
+            id_pago INT PRIMARY KEY AUTO_INCREMENT,
+            id_reserva INT(5),
+            metodo_pago VARCHAR(30),
+            estado VARCHAR(30));");
+
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS asiento_sesion(
+            id_asiento_sesion INT PRIMARY KEY AUTO_INCREMENT,
+            id_asiento INT(5),
+            id_sesion INT(5),
+            estado VARCHAR(30));");
+
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS sesion(
+            id_sesion INT PRIMARY KEY AUTO_INCREMENT,
+            id_pelicula INT(5),
+            id_sala INT(5),
+            fecha_hora VARCHAR(10));");
         
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS pelicula(
+            id_pelicula INT PRIMARY KEY AUTO_INCREMENT,
+            titulo VARCHAR(50),
+            sinopsis VARCHAR(300),
+            duracion VARCHAR(20),
+            genero VARCHAR(30),
+            clasificacion VARCHAR(30),
+            imagen VARCHAR(100));");
 
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS sala(
+            id_sala INT PRIMARY KEY AUTO_INCREMENT,
+            nombre VARCHAR(20),
+            capacidad INT(3));");
 
+        $bdConexion->exec("CREATE TABLE IF NOT EXISTS asiento(
+            id_asiento INT PRIMARY KEY AUTO_INCREMENT,
+            id_sala INT(5),
+            fila INT(2),
+            numero INT(2));");
+        
+        
 
 
 
