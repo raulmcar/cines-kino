6d4e3fb72ed62e085ff27a49aad4b9dbd26fb807
Raul <raulmcar@hotmail.com>
2025-03-17 03:26:15 +0100

    Haciendo un poco del index

--- index.php
+++ index.php
@@ -7,9 +7,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>    
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/index.css">
     <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar bg-dark-subtle">
+        <div class="container d-flex justify-content-between align-items-center">
+            <a class="navbar-brand" href="">
+                <img src="./imagenes/logo.png" alt="Bootstrap" width="110" height="80">
+            </a>
+            <a class="navbar-brand fs-4 fw-semibold" href="#">Cartelera</a>
+            <button type="button" class="btn btn-warning text-black rounded-circle">
+                <i class="bi bi-person-fill fs-3"></i>
+            </button>
+        </div>
+    </nav>
+
     
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
