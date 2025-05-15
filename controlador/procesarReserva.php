7c4d0aedde9676aafc7ec6c27f0901ffad0fd939
Raul <raulmcar@hotmail.com>
2025-05-15 23:35:03 +0200

    crear reserva

--- controlador/procesarReserva.php
+++ controlador/procesarReserva.php
@@ -1,8 +1,22 @@
 <?php
     session_start();
+    require_once('../modelo/usuario.php');
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        
+        if (isset($_POST['correo_reserva'], $_POST['contrasena_reserva'])){
+
+            $usuario = Usuario::iniciarSesion($_POST['correo_reserva'], $_POST['contrasena_reserva']);
+
+            if (!$usuario){
+                $_SESSION['msg'] = "Ha habido un problema a la hora de inciar sesión";
+                header('Location: ../vista/pagarReserva.php');
+                exit();
+            } else {
+                
+            }
+        }
+    } else {
+
     }
 
 ?>
--- modelo/reserva.php
+++ modelo/reserva.php
@@ -2,12 +2,12 @@
     require_once('bd.php');
 
     class Reserva{
-        private int $id_usuario;
+        private ?int $id_usuario;
         private int $id_sesion;
         private float $precio;
         private string $fechaReserva;
 
-        public function __construct(int $id_usuario, int $id_sesion, float $precio, string $fechaReserva){
+        public function __construct(?int $id_usuario, int $id_sesion, float $precio, string $fechaReserva){
             $this->id_usuario = $id_usuario;
             $this->id_sesion = $id_sesion;
             $this->precio = $precio;
@@ -21,16 +21,33 @@
             $this->fechaReserva = "";
         }
 
-        public function crearReserva(){
+        public function crearReserva() {
+            $registro = false;
 
+            try {
+                $pdo = new BD();
+                $bdConexion = $pdo->getPDO();
+
+                $consulta = $bdConexion->prepare("INSERT INTO reserva (id_usuario, id_sesion, precio, fecha_reserva)
+                    VALUES (?, ?, ?, ?)");
+
+                if ($this->id_usuario === null) {
+                    $consulta->bindValue(1, null);
+                } else {
+                    $consulta->bindValue(1, $this->id_usuario);
+                }
+
+                $consulta->bindParam(2, $this->id_sesion);
+                $consulta->bindParam(3, $this->precio);
+                $consulta->bindParam(4, $this->fechaReserva);
+
+                $consulta->execute();
+                $registro = true;
+                return $registro;
+            } catch (PDOException $e) {
+                echo "Error al hacer la reserva: " . $e->getMessage();
+                return $registro;
+            }
         }
-
-
-
-
-
-
-
     }
-
 ?>
