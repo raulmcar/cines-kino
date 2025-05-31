a4656a6d1cb9ef7469f1a29ebde03524d4a5d6a7
raulmcar <raulmcar@hotmail.com>
2025-05-31 23:29:37 +0200

    ultimos ajustes

--- vista/iniciadoAdmin.php
+++ vista/iniciadoAdmin.php
@@ -26,8 +26,13 @@
     <link rel="stylesheet" href="../css/index.css?v=1.0">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+    </style>
 </head>
-<body class="d-flex flex-column min-vh-100"> <!-- VER PORQUE SALE LA OPCION DE HACER SCROLL HORIZONTAL -->
+<body class="d-flex flex-column min-vh-100"> 
     <?php include '../include/navbar.php'; ?>
 
     <div class="row d-flex justify-content-center mt-5 mb-5">
--- vista/pagarReserva.php
+++ vista/pagarReserva.php
@@ -25,6 +25,11 @@
     <link rel="stylesheet" href="/cine/css/index.css?v=1.0">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+    </style>
 </head>
 <body class="d-flex flex-column min-vh-100">
     <?php include '../include/navbar.php'; ?>
